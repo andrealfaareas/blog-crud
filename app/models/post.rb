bff8051913b5b508603f78d99c10b77344e9ba9d
Andre LFA <andrelfaareas@gmail.com>
2017-12-27 18:34:35 -0200

    Add bootstrap simple_form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates_presence_of :title, :message => " - Deve ser preenchido"
+  validates_presence_of :body, :message => " - Deve ser preenchido"
+  validates_presence_of :category, :message => " - Deve ser preenchido"
 end
